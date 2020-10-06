0330450be2a784c49d3a1877c1a3776af88d7a7b
Bernardino Villagra <bernardinovillagra@hotmail.com>
2020-10-06 12:46:02 -0300

    Ciclo Do While en Java

---
+++ CicloDoWhile/Main.java
@@ -0,0 +1,12 @@
+package CicloDoWhile;
+
+public class Main {
+  public static void main(String[] args) {
+    // Ciclo Do While
+    int contador = 100;
+    do {
+      System.out.println("Ejecución del Do " + contador);
+      contador++;
+    } while (contador < 10);
+  }
+}
